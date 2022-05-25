92152e1288229e4e7d5508d02db02b63631aea3c
Branko <brankos.vratonjic@gmail.com>
2022-05-25 14:49:25 +0200

    Added grid and color optical flow of the whole image

--- utility.py
+++ utility.py
@@ -10,6 +10,10 @@ from torch import square
 
 ### PYRAMIDS
 def create_gaussian_image_pyramid(image, pyramid_levels):
+    height, width = image.shape[0:2]
+    #we must have the rounded values in order to reconstruct the pyramid
+    assert height % 2**pyramid_levels == 0
+    assert width % 2**pyramid_levels == 0
     gauss_copy = np.ndarray(shape=image.shape, dtype="float")
     gauss_copy[:] = image
     img_pyramid = [gauss_copy]
@@ -148,7 +152,7 @@ def show_image_nb(image):
     if len(image.shape)==2:
         pyplot.imshow(image,cmap='gray', vmin=0, vmax=255)
     else:
-        pyplot.imshow(image)
+        pyplot.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
 def draw_roi(image, square_coords):
     start_point = (square_coords['y1'],square_coords['x1'])
@@ -164,6 +168,8 @@ def draw_roi_nb(image, square_coords):
     show_image_nb(image_drawn)
 
 def show_optical_flow_roi(video_input, base_point, square_coords, amplitude):
+    if os.path.exists(video_input) == False:
+        raise Exception('The path to the video does not exist!')
     cap = cv2.VideoCapture(video_input)
     ret, first_frame = cap.read()
     prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
@@ -324,3 +330,187 @@ def get_capture_dimensions(cap):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     return width, height
+
+
+
+### OPTICAL FLOW FUNCTIONS 
+
+## Approach 1: COLOR
+
+def make_colorwheel():
+    """
+    Generates a color wheel for optical flow visualization as presented in:
+        Baker et al. "A Database and Evaluation Methodology for Optical Flow" (ICCV, 2007)
+        URL: http://vision.middlebury.edu/flow/flowEval-iccv07.pdf
+    Code follows the original C++ source code of Daniel Scharstein.
+    Code follows the the Matlab source code of Deqing Sun.
+    Returns:
+        np.ndarray: Color wheel
+    """
+
+    RY = 15
+    YG = 6
+    GC = 4
+    CB = 11
+    BM = 13
+    MR = 6
+
+    ncols = RY + YG + GC + CB + BM + MR
+    colorwheel = np.zeros((ncols, 3))
+    col = 0
+
+    # RY
+    colorwheel[0:RY, 0] = 255
+    colorwheel[0:RY, 1] = np.floor(255*np.arange(0,RY)/RY)
+    col = col+RY
+    # YG
+    colorwheel[col:col+YG, 0] = 255 - np.floor(255*np.arange(0,YG)/YG)
+    colorwheel[col:col+YG, 1] = 255
+    col = col+YG
+    # GC
+    colorwheel[col:col+GC, 1] = 255
+    colorwheel[col:col+GC, 2] = np.floor(255*np.arange(0,GC)/GC)
+    col = col+GC
+    # CB
+    colorwheel[col:col+CB, 1] = 255 - np.floor(255*np.arange(CB)/CB)
+    colorwheel[col:col+CB, 2] = 255
+    col = col+CB
+    # BM
+    colorwheel[col:col+BM, 2] = 255
+    colorwheel[col:col+BM, 0] = np.floor(255*np.arange(0,BM)/BM)
+    col = col+BM
+    # MR
+    colorwheel[col:col+MR, 2] = 255 - np.floor(255*np.arange(MR)/MR)
+    colorwheel[col:col+MR, 0] = 255
+    return colorwheel
+
+
+def flow_uv_to_colors(u, v, convert_to_bgr=False):
+    """
+    Applies the flow color wheel to (possibly clipped) flow components u and v.
+    According to the C++ source code of Daniel Scharstein
+    According to the Matlab source code of Deqing Sun
+    Args:
+        u (np.ndarray): Input horizontal flow of shape [H,W]
+        v (np.ndarray): Input vertical flow of shape [H,W]
+        convert_to_bgr (bool, optional): Convert output image to BGR. Defaults to False.
+    Returns:
+        np.ndarray: Flow visualization image of shape [H,W,3]
+    """
+    flow_image = np.zeros((u.shape[0], u.shape[1], 3), np.uint8)
+    colorwheel = make_colorwheel()  # shape [55x3]
+    ncols = colorwheel.shape[0]
+    rad = np.sqrt(np.square(u) + np.square(v))
+    a = np.arctan2(-v, -u)/np.pi
+    fk = (a+1) / 2*(ncols-1)
+    k0 = np.floor(fk).astype(np.int32)
+    k1 = k0 + 1
+    k1[k1 == ncols] = 0
+    f = fk - k0
+    for i in range(colorwheel.shape[1]):
+        tmp = colorwheel[:,i]
+        col0 = tmp[k0] / 255.0
+        col1 = tmp[k1] / 255.0
+        col = (1-f)*col0 + f*col1
+        idx = (rad <= 1)
+        col[idx]  = 1 - rad[idx] * (1-col[idx])
+        col[~idx] = col[~idx] * 0.75   # out of range
+        # Note the 2-i => BGR instead of RGB
+        ch_idx = 2-i if convert_to_bgr else i
+        flow_image[:,:,ch_idx] = np.floor(255 * col)
+    return flow_image
+
+
+def flow_to_color(flow_uv, clip_flow=None, convert_to_bgr=False):
+    """
+    Expects a two dimensional flow image of shape.
+    Args:
+        flow_uv (np.ndarray): Flow UV image of shape [H,W,2]
+        clip_flow (float, optional): Clip maximum of flow values. Defaults to None.
+        convert_to_bgr (bool, optional): Convert output image to BGR. Defaults to False.
+    Returns:
+        np.ndarray: Flow visualization image of shape [H,W,3]
+    """
+    assert flow_uv.ndim == 3, 'input flow must have three dimensions'
+    assert flow_uv.shape[2] == 2, 'input flow must have shape [H,W,2]'
+    if clip_flow is not None:
+        flow_uv = np.clip(flow_uv, 0, clip_flow)
+    u = flow_uv[:,:,0]
+    v = flow_uv[:,:,1]
+    rad = np.sqrt(np.square(u) + np.square(v))
+    rad_max = np.max(rad)
+    epsilon = 1e-5
+    u = u / (rad_max + epsilon)
+    v = v / (rad_max + epsilon)
+    return flow_uv_to_colors(u, v, convert_to_bgr)
+
+def show_optical_flow_color(video_input, clip_flow=None, convert_to_bgr=False):
+    if os.path.exists(video_input) == False:
+        raise Exception('The path to the video does not exist!')
+    cap = cv2.VideoCapture(video_input)
+    ret, first_frame = cap.read()
+    prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+
+    while(cap.isOpened()):
+        # READING IMAGE BY IMAGE
+        ret, frame = cap.read()
+        if ret is False:
+                break
+        # CONVERTING TO GRAYSCALE
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # CALCULATING FLOW
+        flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        image = flow_to_color(flow, clip_flow=clip_flow, convert_to_bgr=convert_to_bgr)
+
+        prev_gray = gray
+
+        # PRESS 'Q' TO EXIT
+        cv2.imshow('arrow', image) 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+## Approach 2: GRID
+def draw_flow(im, flow, step=50, amp=2):
+    h,w = im.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype('int')
+    fx, fy = flow[y,x].T
+
+    #create line endpoints
+    lines = np.vstack([x,y,x+fx*amp,y+fy*amp]).T.reshape(-1,2,2)
+    lines = np.int32(lines)
+
+    #create image and draw
+    vis = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
+    for (x1,y1), (x2,y2) in lines:
+        cv2.arrowedLine(vis, (x1,y1),(x2,y2),(0,255,0),2)
+        cv2.circle(vis,(x1,y1),1,(0,255,0),-1)
+    return vis
+    
+
+def show_optical_flow_grid(video_input, step=50, amp=2):
+    if os.path.exists(video_input) == False:
+        raise Exception('The path to the video does not exist!')
+    cap = cv2.VideoCapture(video_input)
+    ret, im = cap.read()
+    prev_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    while True:
+        ret, im = cap.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+        #compute flow
+        flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3,15,3,5,1.2,0)
+        prev_gray = gray
+
+        #plot the flow
+        cv2.imshow('Optical flow',draw_flow(gray, flow, step=step, amp=amp))
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
